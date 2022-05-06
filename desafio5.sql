c32164b6709b989220c99f347fa20a12d41bf212
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-05-06 20:08:28 -0300

    feat: As 2 musicas mais tocadas. Requisito 5

---
+++ desafio5.sql
@@ -0,0 +1,6 @@
+SELECT s.name_song AS 'cancao', COUNT(h.song_id) AS 'reproducoes' FROM SpotifyClone.Songs AS s
+INNER JOIN SpotifyClone.History AS h
+ON s.id = h.song_id
+GROUP BY s.name_song
+ORDER BY reproducoes DESC
+LIMIT 2;
