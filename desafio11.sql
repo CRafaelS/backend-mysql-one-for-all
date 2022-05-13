1315979ec9a590aeafdef1bed330380e3b281b63
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-05-12 22:05:54 -0300

    feat: alterando o nome de algumas musicas

---
+++ desafio11.sql
@@ -0,0 +1,11 @@
+SELECT name_song AS nome_musica,
+	CASE
+    WHEN name_song LIKE '%Streets' THEN REPLACE(name_song,'Streets', 'Code Review')
+    WHEN name_song LIKE '%Her Own' THEN REPLACE(name_song,'Her Own', 'Trybe')
+    WHEN name_song LIKE '%Inner Fire' THEN REPLACE(name_song,'Inner Fire', 'Project')
+    WHEN name_song LIKE '%Silly' THEN REPLACE(name_song,'Silly', 'Nice')
+    WHEN name_song LIKE '%Circus' THEN REPLACE(name_song,'Circus', 'Pull Request')
+    END AS novo_nome
+FROM SpotifyClone.Songs
+HAVING novo_nome IS NOT NULL
+ORDER BY nome_musica;
